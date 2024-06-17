042fe40bf30ab844070f338f29fed432c9f00161
Anderson Oliveira <andersonreinilson@hotmail.com>
2024-06-16 22:57:15 -0300

    HEY-7: wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -20,7 +20,7 @@
                                 border border-gray-300 focus:ring-blue-500 focus:border-blue-500
                               dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
                             "
-                            placeholder="Ask my anything...">{{ old('question') }}</textarea>
+                            placeholder="Ask me anything...">{{ old('question') }}</textarea>
                             @error('question')
                                 <span class="text-red-400">{{$message}}</span>
                             @enderror
